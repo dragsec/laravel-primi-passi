52cc2baf37448bc58a0c4e2f95474e29cfa3ba5c
dragsec <draganyes@gmail.com>
2021-06-17 17:18:23 +0200

    test page

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -4,12 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel Homepage</title>
+    <title>test</title>
 </head>
 <body>
-    <h1>Hello World</h1>
-    <h3>Go to <a href="/test">test page</a></h3>
-    
-
+    test
 </body>
 </html>
